5c032aa746962de329a48a77c906bd8a92d92246
serglo06 <serglo06@ucm.es>
2025-02-27 20:01:31 +0100

    Copias

---
+++ Assets/Scripts/Enemies/Enemy2_attack.cs
@@ -0,0 +1,135 @@
+//---------------------------------------------------------
+// Breve descripción del contenido del archivo
+// Responsable de la creación de este archivo
+// Nombre del juego
+// Proyectos 1 - Curso 2024-25
+//---------------------------------------------------------
+
+using System.Collections;
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class Enemy2_attack : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+
+    [SerializeField] int Damage;   //Cantidad de vidas que resta el enemigo al jugador con cada golpe de su ataque
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+
+    GameObject _hitbox;             //Objeto que contiene el collider del ataque (desde ahora será llamado "hitbox" en los comentarios)
+    Enemy_movement _mov;            //Script de movimiento del enemigo
+    Vector2 _dir;                   //Dirección en la que se mueve el enemigo
+
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start() {
+        _hitbox = transform.GetChild(0).gameObject;
+        _mov = GetComponent<Enemy_movement>();
+        _hitbox.SetActive(false);
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update() {
+        if (!_hitbox.activeSelf)
+        {
+            _dir = _mov.GetDir();
+            SetDir(_dir, 0.25f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.GetComponent<Player_Health>() != null)
+        {
+            collision.GetComponent<Player_Health>().Damage(Damage);
+        }
+    }
+
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+
+
+
+    /// <summary>
+    /// Método que llama a la corrutina encargada de activar la hitbox y la desactiva pasado un breve lapso de tiempo.
+    /// </summary>
+    public void Attack() {
+        StartCoroutine(AttackCoroutine());
+    }
+
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+
+    /// <summary>
+    /// Método que dictamina la posición y rotación de la hitbox del ataque
+    /// </summary>
+    /// <param name="v"> Vector de dirección del jugador. Es usado para determinar la posición y rotación de la hitbox </param>
+    /// <param name="offset"> Float que indica cuánto desplazamos la hitbox con respecto al centro del enemigo </param>
+    void SetDir(Vector2 v, float offset) {
+        Vector2 res;
+        _hitbox.transform.rotation = transform.rotation;
+
+        if (v.y == 0)
+        {
+            if (v.x > 0) res = new Vector2(offset, 0);
+            else if (v.x < 0) res = new Vector2(-offset, 0);
+            else res = Vector2.zero;
+        }
+        else
+        {
+            if (v.y > 0) res = new Vector2(0, offset);
+            else res = new Vector2(0, -offset);
+
+            _hitbox.transform.Rotate(0, 0, 90, Space.World);
+
+        }
+        _hitbox.transform.localPosition = res;
+    }
+
+    /// <summary>
+    /// Método que deshabilita la hitbox
+    /// </summary>
+    void DisableHitbox() {
+        _hitbox.SetActive(false);
+    }
+
+    /// <summary>
+    /// Corrutina que activa y desactiva la hitbox 3 veces seguidas
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator AttackCoroutine() {
+        for (int i = 0; i < 3; i++)
+        {
+            _hitbox.SetActive(true);
+            Invoke(nameof(DisableHitbox), 0.25f);
+            yield return new WaitForSecondsRealtime(0.3f);
+        }
+    }
+
+    #endregion   
+
+
+} // class Enemy2_attack 
+// namespace
---
+++ Assets/Scripts/Enemies/Enemy2_movement.cs
@@ -0,0 +1,197 @@
+//---------------------------------------------------------
+// Breve descripción del contenido del archivo
+// Responsable de la creación de este archivo
+// Nombre del juego
+// Proyectos 1 - Curso 2024-25
+//---------------------------------------------------------
+
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class Enemy2_movement : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+
+    [SerializeField] float MovementSpeed;           //Velocidad de movimiento del enemigo
+    [SerializeField] float RestTime;                //Tiempo que el enemigo pasa quieto después de atacar
+    [SerializeField] float AttackCooldown;          //Tiempo que pasa entre cada uno de los tres ataques
+
+    #endregion
+
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+
+    Enemy1_attack AttackScript;
+    SpriteRenderer _spriteRend;
+    GameObject _player;                     //Jugador en la escena
+    Rigidbody2D _rb;                        //Componente rigidBody del enemigo
+    Animator _anim;                         //Componente animator del enemigo
+    Vector2 _dir = Vector2.zero;            //Vector que marca la dirección hacia la que se mueve el enemigo
+    float _restTimer = 0f;                  //Temporizador que cuenta el tiempo de descanso
+    float _dirTimer = 0f;                   //Temporizador que cuenta el tiempo necesario para que el enemigo cambie su dirección de movimiento
+    int _rangeLayer;                        //Entero que representa la capa donde esta el objeto que marca el rango efctivo del enemigo
+    bool _isResting = false;                //Bandera que marca si el enemigo está descansando
+    bool _onRange = false;                  //Bandera que marca si el enemigo está a la distancia necesaria del jugador para ejecutar su ataque
+
+    #endregion
+
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    void Start() {
+        _player = FindObjectOfType<Movement>().gameObject;
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        AttackScript = GetComponent<Enemy1_attack>();
+        _rangeLayer = FindObjectOfType<Enemy_range>().gameObject.layer;
+        _spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate() {
+        _rb.velocity = Vector2.zero;
+        if (!_isResting)
+        {
+            Vector2 player_position = _player.transform.position;
+            if (!_onRange)
+            {
+
+                if (_dirTimer >= 0.25f)
+                {
+                    _dir = GetDirection(player_position - (Vector2)transform.position);
+                    _dirTimer = 0f;
+                }
+                else _dirTimer += Time.deltaTime;
+
+                _rb.velocity = _dir * MovementSpeed;
+            }
+            else
+            {
+                _dir = GetDirection(player_position - (Vector2)transform.position);
+                _rb.velocity = _dir * 0.001f;
+                AttackScript.Attack();
+                _anim.SetTrigger("_Attack");
+                _isResting = true;
+            }
+        }
+        else
+        {
+            _restTimer += Time.deltaTime;
+
+            if (_restTimer >= RestTime)
+            {
+                _restTimer = 0f;
+                _isResting = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.layer == _rangeLayer) _onRange = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.layer == _rangeLayer) _onRange = false;
+    }
+
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+
+    public Vector2 GetDir() {
+        return _dir;
+    }
+
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    /// <summary>
+    /// Esta función toma un vector y calcula su dirección (8 direcciones posibles).
+    /// Tiene un poco de margen en las direcciones no diagonales.
+    /// También llama al método privado SetAnim.
+    /// </summary>
+    /// <param name="v"> Vector cuya dirección se quiere calcular </param>
+    /// <returns></returns>
+    Vector2 GetDirection(Vector2 v) {
+        Vector2 res;
+        float ang = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        ang = (ang + 360) % 360;
+
+        //Derecha
+        if (ang <= 15 && ang >= 0 || ang <= 360 && ang >= 345) res = Vector2.right;
+        //Arriba
+        else if (ang <= 105 && ang >= 75) res = Vector2.up;
+        //Izquierda
+        else if (ang <= 195 && ang >= 165) res = Vector2.left;
+        //Abajo
+        else if (ang <= 285 && ang >= 255) res = Vector2.down;
+        //1er cuadrante
+        else if (ang <= 90 && ang >= 0) res = (Vector2.right + Vector2.up).normalized;
+        //2do cuadrante
+        else if (ang <= 180 && ang > 90) res = (Vector2.up + Vector2.left).normalized;
+        //3er cuadrante
+        else if (ang <= 270 && ang > 180) res = (Vector2.left + Vector2.down).normalized;
+        //4to cuadrante
+        else res = (Vector2.down + Vector2.right).normalized;
+
+        SetAnim(res);
+
+        return res;
+    }
+
+    /// <summary>
+    /// Dado un vector, lo evalua y se encarga de asignar el valor correcto a los booleanos del controlador de animaciones del enemigo.
+    /// También se encarga de voltear el sprite si es necesario.
+    /// </summary>
+    /// <param name="v"> Vector que va a ser evaluado </param>
+    void SetAnim(Vector2 v) {
+        _spriteRend.flipX = false;
+
+        string side = "_MvSide";
+        string up = "_MvUp";
+        string down = "_MvDown";
+
+        if (v.x > 0)
+        {
+            _anim.SetBool(side, true);
+        }
+        else if (v.x < 0)
+        {
+            _anim.SetBool(side, true);
+            _spriteRend.flipX = true;
+
+        }
+        else
+        {
+            _anim.SetBool(side, false);
+        }
+
+        if (v.y > 0)
+        {
+            _anim.SetBool(up, true);
+            _anim.SetBool(down, false);
+        }
+        else if (v.y < 0)
+        {
+            _anim.SetBool(up, false);
+            _anim.SetBool(down, true);
+        }
+        else
+        {
+            _anim.SetBool(up, false);
+            _anim.SetBool(down, false);
+        }
+    }
+
+    #endregion
+} // class Enemy2_movement 
+// namespace
