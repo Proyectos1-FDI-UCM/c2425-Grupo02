4d15c52041e0ddace04045cf08898cc0429f9ce9
Macarr0n <adri.arbas@gmail.com>
2025-03-01 20:10:53 +0100

    Enemigo 2 (prácticamente acabado)
    Falta solamente ajustar la hitbox y que te vuelva a perseguir y ya

--- Assets/Scripts/Enemies/Enemy2_movement.cs
+++ Assets/Scripts/Enemies/Enemy2_movement.cs
@@ -5,6 +5,8 @@
 // Proyectos 1 - Curso 2024-25
 //---------------------------------------------------------
 
+using System;
+using System.Collections;
 using UnityEngine;
 // Añadir aquí el resto de directivas using
 
@@ -28,7 +30,7 @@ public class Enemy2_movement : MonoBehaviour
     // ---- ATRIBUTOS PRIVADOS ----
     #region Atributos Privados (private fields)
 
-    Enemy1_attack AttackScript;
+    Enemy2_attack AttackScript;
     SpriteRenderer _spriteRend;
     GameObject _player;                     //Jugador en la escena
     Rigidbody2D _rb;                        //Componente rigidBody del enemigo
@@ -36,9 +38,11 @@ public class Enemy2_movement : MonoBehaviour
     Vector2 _dir = Vector2.zero;            //Vector que marca la dirección hacia la que se mueve el enemigo
     float _restTimer = 0f;                  //Temporizador que cuenta el tiempo de descanso
     float _dirTimer = 0f;                   //Temporizador que cuenta el tiempo necesario para que el enemigo cambie su dirección de movimiento
+    float _attackAgainTimer = 5f;           //Temporizador que cuenta el tiempo necesario para que el enemigo cambie su estado de huir a ataque
     int _rangeLayer;                        //Entero que representa la capa donde esta el objeto que marca el rango efctivo del enemigo
     bool _isResting = false;                //Bandera que marca si el enemigo está descansando
     bool _onRange = false;                  //Bandera que marca si el enemigo está a la distancia necesaria del jugador para ejecutar su ataque
+    bool _rechargingAttack = false;         //Bandera que marca si el enemigo está en la fase de recargar su ataque
 
     #endregion
 
@@ -50,14 +54,14 @@ public class Enemy2_movement : MonoBehaviour
         _player = FindObjectOfType<Movement>().gameObject;
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
-        AttackScript = GetComponent<Enemy1_attack>();
+        AttackScript = GetComponent<Enemy2_attack>();
         _rangeLayer = FindObjectOfType<Enemy_range>().gameObject.layer;
         _spriteRend = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate() {
         _rb.velocity = Vector2.zero;
-        if (!_isResting)
+        if (!_isResting && !_rechargingAttack)
         {
             Vector2 player_position = _player.transform.position;
             if (!_onRange)
@@ -65,7 +69,7 @@ public class Enemy2_movement : MonoBehaviour
 
                 if (_dirTimer >= 0.25f)
                 {
-                    _dir = GetDirection(player_position - (Vector2)transform.position);
+                    _dir = GetDirection_AttackTime(player_position - (Vector2)transform.position);
                     _dirTimer = 0f;
                 }
                 else _dirTimer += Time.deltaTime;
@@ -74,14 +78,14 @@ public class Enemy2_movement : MonoBehaviour
             }
             else
             {
-                _dir = GetDirection(player_position - (Vector2)transform.position);
+                _dir = GetDirection_AttackTime(player_position - (Vector2)transform.position);
                 _rb.velocity = _dir * 0.001f;
                 AttackScript.Attack();
                 _anim.SetTrigger("_Attack");
                 _isResting = true;
             }
         }
-        else
+        else if (_isResting && !_rechargingAttack)
         {
             _restTimer += Time.deltaTime;
 
@@ -89,7 +93,23 @@ public class Enemy2_movement : MonoBehaviour
             {
                 _restTimer = 0f;
                 _isResting = false;
+                _rechargingAttack = true;
             }
+        }
+        else if (!_isResting && _rechargingAttack)
+        {
+            Vector2 player_position = _player.transform.position;
+
+            if (_dirTimer >= 0.25f)
+            {
+                _dir = GetDirection_RunawayTime(player_position - (Vector2)transform.position);
+                _dirTimer = 0f;
+            }
+            else _dirTimer += Time.deltaTime;
+
+            _rb.velocity = _dir * MovementSpeed;
+            CooldownRoutine();
+            Console.Write(_attackAgainTimer);
         }
     }
 
@@ -121,7 +141,7 @@ public class Enemy2_movement : MonoBehaviour
     /// </summary>
     /// <param name="v"> Vector cuya dirección se quiere calcular </param>
     /// <returns></returns>
-    Vector2 GetDirection(Vector2 v) {
+    Vector2 GetDirection_AttackTime(Vector2 v) {
         Vector2 res;
         float ang = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
         ang = (ang + 360) % 360;
@@ -142,6 +162,34 @@ public class Enemy2_movement : MonoBehaviour
         else if (ang <= 270 && ang > 180) res = (Vector2.left + Vector2.down).normalized;
         //4to cuadrante
         else res = (Vector2.down + Vector2.right).normalized;
+
+        SetAnim(res);
+
+        return res;
+    }
+
+    Vector2 GetDirection_RunawayTime(Vector2 v)
+    {
+        Vector2 res;
+        float ang = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        ang = (ang + 360) % 360;
+
+        //Derecha
+        if (ang <= 15 && ang >= 0 || ang <= 360 && ang >= 345) res = Vector2.left;
+        //Arriba
+        else if (ang <= 105 && ang >= 75) res = Vector2.down;
+        //Izquierda
+        else if (ang <= 195 && ang >= 165) res = Vector2.right;
+        //Abajo
+        else if (ang <= 285 && ang >= 255) res = Vector2.up;
+        //1er cuadrante
+        else if (ang <= 90 && ang >= 0) res = (Vector2.left + Vector2.down).normalized;
+        //2do cuadrante
+        else if (ang <= 180 && ang > 90) res = (Vector2.down + Vector2.right).normalized;
+        //3er cuadrante
+        else if (ang <= 270 && ang > 180) res = (Vector2.right + Vector2.up).normalized;
+        //4to cuadrante
+        else res = (Vector2.up + Vector2.left).normalized;
 
         SetAnim(res);
 
@@ -190,6 +238,11 @@ public class Enemy2_movement : MonoBehaviour
             _anim.SetBool(up, false);
             _anim.SetBool(down, false);
         }
+    }
+    IEnumerator CooldownRoutine()
+    {
+        yield return new WaitForSeconds(_attackAgainTimer);
+        _rechargingAttack = false;
     }
 
     #endregion
