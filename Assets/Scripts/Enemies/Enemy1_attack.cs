e4c52bdea437f439006982c969b2d6499b23a1dd
serglo06 <serglo06@ucm.es>
2025-03-24 19:21:56 +0100

    Refactorización del spawn, cambio de la hitbox del enemigo1 y cambio al script de salud de los enemigos
    La hitbox del enemigo 1 ahora es redonda y ya no rota.
    
    Ahora se pueden instanciar enemigos sin spawn (no lo recomiendo por cuestiones de diseño)
    
    Problema del array del spawn arreglado, ahora el codigo deberia ser mas eficiente

--- Assets/Scripts/Enemies/Enemy1_attack.cs
+++ Assets/Scripts/Enemies/Enemy1_attack.cs
@@ -77,11 +77,12 @@ public class Enemy1_Attack : MonoBehaviour, IAttack
     /// <summary>
     /// Si la hitbox colisiona con el jugador, llama el método para dañarle
     /// </summary>
-    /// <param name="collision"></param>
+    /// <param name="collision"> Colisión del objeto con el que se ha colisionado </param>
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.GetComponent<Player_Health>() != null)
+        Player_Health health = collision.GetComponent<Player_Health>();
+        if (health != null)
         {
-            collision.GetComponent<Player_Health>().Damage(Damage);
+            health.Damage(Damage);
         }
     }
 
@@ -123,14 +124,29 @@ public class Enemy1_Attack : MonoBehaviour, IAttack
 
         if(v.y == 0)
         {
-            if (v.x > 0) res = new Vector2(offset, 0);
-            else if (v.x < 0) res = new Vector2(-offset, 0);
-            else res = Vector2.zero;
+            if (v.x > 0)
+            {
+                res = new Vector2(offset, 0);
+            }
+            else if (v.x < 0)
+            {
+                res = new Vector2(-offset, 0);
+            }
+            else
+            {
+                res = Vector2.zero;
+            }
         }
         else
         {
-            if (v.y > 0) res = new Vector2(0, offset);
-            else res = new Vector2(0, -offset);
+            if (v.y > 0)
+            {
+                res = new Vector2(0, offset);
+            }
+            else
+            {
+                res = new Vector2(0, -offset);
+            }
         }
         _hitbox.transform.localPosition = res;
     }
