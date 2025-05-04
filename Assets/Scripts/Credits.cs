8cab9cc1c2e3b029df4d534069e8be1db098013f
Pablo <pcayuela@ucm.es>
2025-05-04 23:17:03 +0200

    Animación extra escena de créditos
    Ahora el logo se desvanece al entrar en los créditos. Aparte se ha limpiado el código de Credits.

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -11,8 +11,7 @@ using UnityEngine.SceneManagement;
 
 
 /// <summary>
-/// Antes de cada class, descripción de qué es y para qué sirve,
-/// usando todas las líneas que sean necesarias.
+/// Permite salir de la escena de créditos, usando el tiempo o presionando x botón
 /// </summary>
 public class Credits : MonoBehaviour
 {
@@ -25,61 +24,46 @@ public class Credits : MonoBehaviour
     // Ejemplo: MaxHealthPoints
 
     #endregion
-    
+
     // ---- ATRIBUTOS PRIVADOS ----
     #region Atributos Privados (private fields)
-    // Documentar cada atributo que aparece aquí.
-    // El convenio de nombres de Unity recomienda que los atributos
-    // privados se nombren en formato _camelCase (comienza con _, 
-    // primera palabra en minúsculas y el resto con la 
-    // primera letra en mayúsculas)
-    // Ejemplo: _maxHealthPoints
-
+    /// <summary>
+    /// Variables del timer
+    /// </summary>
+    private float _animtime = 0f;
+    private float _animlim = 19f;
     #endregion
-    
+
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
     #region Métodos de MonoBehaviour
-    
-    // Por defecto están los típicos (Update y Start) pero:
-    // - Hay que añadir todos los que sean necesarios
-    // - Hay que borrar los que no se usen 
-    
-    /// <summary>
-    /// Start is called on the frame when a script is enabled just before 
-    /// any of the Update methods are called the first time.
-    /// </summary>
-    void Start()
-    {
-        Invoke("WaitToEnd", 19);
-    }
-
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
     void Update()
     {
-        if (InputManager.Instance.PauseWasPressedThisFrame())
+        if (InputManager.Instance.PauseWasPressedThisFrame() || _animtime>= _animlim)
         {
             WaitToEnd();
+            _animtime = 0;
+        }
+        else
+        {
+            _animtime += Time.deltaTime;
         }
     }
     #endregion
 
     // ---- MÉTODOS PÚBLICOS ----
     #region Métodos públicos
-    // Documentar cada método que aparece aquí con ///<summary>
-    // El convenio de nombres de Unity recomienda que estos métodos
-    // se nombren en formato PascalCase (palabras con primera letra
-    // mayúscula, incluida la primera letra)
-    // Ejemplo: GetPlayerController
+
     #endregion
-    
+
     // ---- MÉTODOS PRIVADOS ----
     #region Métodos Privados
-    // Documentar cada método que aparece aquí
-    // El convenio de nombres de Unity recomienda que estos métodos
-    // se nombren en formato PascalCase (palabras con primera letra
-    // mayúscula, incluida la primera letra)
+
+    /// <summary>
+    /// Carga la escena Main_Menu
+    /// </summary>
     private void WaitToEnd()
     {
         SceneManager.LoadScene("Main_Menu");
