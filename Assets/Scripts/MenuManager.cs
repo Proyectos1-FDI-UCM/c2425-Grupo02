0f4496d518427d131243f664f31507813a6753fc
DarkSideOficial <rcena@ucm.es>
2025-06-30 13:16:41 +0200

    Arreglos finales

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -97,7 +97,6 @@ public class MenuManager : MonoBehaviour
     }
     public void GoCredits()
     {
-        SaveSystem.DeleteSaveData();
         UnityEngine.SceneManagement.SceneManager.LoadScene("Credits");
         System.GC.Collect();
     }
